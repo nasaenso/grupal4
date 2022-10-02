814ea21892a1c7ca403e4cc1561a9b0954ab9028
nasaenso <nahuelsaenzsosa@gmail.com>
2022-10-02 11:05:15 -0300

    commit 3

--- js/script.js
+++ js/script.js
@@ -1,18 +1,16 @@
 const urlPeliculas = "https://japceibal.github.io/japflix_api/movies-data.json";
-peliculasArray = [];
+let peliculasArray = [];
+let empty = [];
 
 function mostrarPeliculas(array){
     let mostrar = "";
 
     for (let pelicula of array){
         mostrar += `
-            <div id="canvas" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">   
-                <li>${pelicula.title}</li>
-                <p>${pelicula.tagline}</p>
-                <span>${puntuacion(Math.round(pelicula.vote_average)/2)}</span>
-                <span> ${generos(pelicula.genres)}</span>
-            </div>
-            <br>
+        <div onclick="offCanvas(${pelicula.id})" class= "card p-3 bg-dark col-md-4 w-auto h-auto list-group-item color" role="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">
+                <h5>${pelicula.title}</h5> <p class="icons position-absolute top-0 mt-3 end-0 me-4">${puntuacion(Math.round(pelicula.vote_average)/1.6)}</p>
+                <i class="text-muted">${pelicula.tagline}</i>
+        </div>
         `
     }
     document.getElementById('lista').innerHTML = mostrar;
@@ -32,12 +30,17 @@ function puntuacion(array){
 }
 
 function buscador(){
-    let textoEscrito = document.getElementById("inputBuscar").value;
-    
+
+    let buscado = " " + document.getElementById("inputBuscar").value;
+
     let listafiltrada = peliculasArray.filter(pelicula => {
-    return (pelicula.title.toLowerCase().indexOf(textoEscrito.toLowerCase()) > -1 )||
-    (pelicula.overview.toLowerCase().indexOf(textoEscrito.toLowerCase()) > -1 )||
-    (pelicula.tagline.toLowerCase().indexOf(textoEscrito.toLowerCase()) > -1) ;})
+    
+        tituloBuscar = pelicula.title.toLowerCase().indexOf(buscado.toLowerCase()) > -1;
+        overviewBuscar = pelicula.overview.toLowerCase().indexOf(buscado.toLowerCase()) > -1;
+        taglineBuscar = pelicula.tagline.toLowerCase().indexOf(buscado.toLowerCase()) > -1;
+        generosBuscar = (generos(pelicula.genres)).toLowerCase().indexOf(buscado.toLowerCase()) > -1;
+
+    return (tituloBuscar) || (overviewBuscar) || (taglineBuscar) || (generosBuscar)})
     mostrarPeliculas(listafiltrada);
 }
 
@@ -49,6 +52,31 @@ function generos(array){
         `
     }
     return nombres;
+}
+function offCanvas(id) {
+
+    let mostrar = "";
+    let pelicula = peliculasArray.find(movie => movie.id == id);
+
+    mostrar = `
+       <li><a class="dropdown-item" href="#">
+        Year: ${(pelicula.release_date).split('-')[0]}</a>
+      </li>
+      <li><a class="dropdown-item" href="#">
+        RunTime: ${pelicula.runtime} minutes </a>
+      </li>
+      <li><a class="dropdown-item" href="#"> 
+        Budget: ${pelicula.budget} </a>
+      </li>
+      <li><a class="dropdown-item" href="#">
+      Revenue: $${pelicula.revenue} </a>
+       </li>
+    `
+
+    document.getElementById('titulo').innerHTML = pelicula.title;
+    document.getElementById('descripcion').innerHTML = pelicula.overview;
+    document.getElementById('generos').innerHTML = generos(pelicula.genres);
+    document.getElementById('info').innerHTML = mostrar;
 }
 
 let obtenerJson = function(url){
@@ -72,6 +100,7 @@ let obtenerJson = function(url){
         return result;
     });
 }
+
 document.addEventListener("DOMContentLoaded", function(){
     obtenerJson(urlPeliculas).then(function(resultObj){
         
@@ -82,7 +111,13 @@ document.addEventListener("DOMContentLoaded", function(){
         }
     });
     document.getElementById('btnBuscar').addEventListener('click',()=>{
-     
-        buscador();
+
+        let textoEscrito = document.getElementById("inputBuscar").value;
+
+        if(textoEscrito == ""){
+            mostrarPeliculas(empty);
+        }else {
+            buscador();
+        }
     });
 });
